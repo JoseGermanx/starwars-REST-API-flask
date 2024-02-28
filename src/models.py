a1e1f8521d5c89e74f747dfa6b6fd79698e6d8ea
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-28 11:27:18 -0300

    Agregadas columnas de nombre y apellido a la tabla de usuarios. Agregado método serialize a la clase Planetas.

--- src/models.py
+++ src/models.py
@@ -6,6 +6,9 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(250), nullable=False)
+    last_name= db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -16,6 +19,8 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
@@ -36,7 +41,18 @@ class Planets(db.Model):
     surface_water = Column(Integer, nullable=False)
 
     def serialize(self):
-        return {}
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water
+        }
 
 class Characters(db.Model):
     __tablename__ = 'characters'
